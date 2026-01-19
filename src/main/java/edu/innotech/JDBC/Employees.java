40f20f3a30697258e02395a7cca0b3fa55010900
Ivan Filippov <ivyfilippov@inno.tech>
2026-01-19 19:48:17 +0500

    refactor code

--- src/main/java/edu/innotech/JDBC/Employees.java
+++ src/main/java/edu/innotech/JDBC/Employees.java
@@ -3,15 +3,23 @@ package edu.innotech.JDBC;
 import edu.innotech.JDBC.classes.Department;
 import edu.innotech.JDBC.classes.Employee;
 import edu.innotech.JDBC.db.DbClient;
+import edu.innotech.JDBC.interfaces.RowMapper;
 
-import java.sql.SQLException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Employees {
+
+    private static final RowMapper<Employee> MAP_TO_EMPLOYEE = r -> new Employee(
+            r.getInt("ID"),
+            r.getString("Name"),
+            r.getInt("DepartmentID"));
+    private static final RowMapper<Department> MAP_TO_DEPARTMENT = r -> new Department(
+            r.getInt("ID"),
+            r.getString("Name"));
     private static final String DB_URL = "C:\\Users\\IvYFilippov\\Desktop\\Office\\Office";
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         moveAnnToHR();
         updateNameIfLowRegister();
         employeesItCount();
@@ -19,27 +27,19 @@ public class Employees {
 
     public static void moveAnnToHR() {
         DbClient db = new DbClient(DB_URL);
-        List<Employee> annsList = db.select("SELECT * FROM Employee WHERE NAME = 'Ann'",
-                r -> new Employee(
-                        r.getInt("ID"),
-                        r.getString("Name"),
-                        r.getInt("DepartmentID")));
+        List<Employee> annsList = db.select("SELECT * FROM Employee WHERE NAME = 'Ann'", MAP_TO_EMPLOYEE);
         if (annsList.size() == 1) {
-            List<Department> hrDepartment = db.select("SELECT * FROM Department WHERE NAME = 'HR'",
-                    r -> new Department(r.getInt("ID"), r.getString("Name")));
+            List<Department> hrDepartment = db.select("SELECT * FROM Department WHERE NAME = 'HR'", MAP_TO_DEPARTMENT);
             db.update("UPDATE Employee " +
                     "SET DepartmentID = ? " +
                     "WHERE ID = ?", hrDepartment.get(0).getID(), annsList.get(0).getID());
+            System.out.println("Сотрудник " + annsList.get(0).getName() + " перемещен в HR отдел");
         }
     }
 
     public static void updateNameIfLowRegister() {
         DbClient db = new DbClient(DB_URL);
-        List<Employee> employees = db.select("SELECT * FROM Employee",
-                        r -> new Employee(
-                                r.getInt("ID"),
-                                r.getString("Name"),
-                                r.getInt("DepartmentID")))
+        List<Employee> employees = db.select("SELECT * FROM Employee", MAP_TO_EMPLOYEE)
                 .stream()
                 .filter(e -> Character.isLowerCase(e.getName().charAt(0)))
                 .peek(e -> e.setName(e.getName().substring(0, 1).toUpperCase() + e.getName().substring(1)))
@@ -58,14 +58,10 @@ public class Employees {
     public static void employeesItCount() {
         DbClient db = new DbClient(DB_URL);
         List<Employee> employees = db.select("SELECT * " +
-                        "FROM Employee e " +
-                        "JOIN Department d " +
-                        "WHERE e.DepartmentID = d.ID " +
-                        "AND d.Name = 'IT' ",
-                r -> new Employee(
-                        r.getInt("ID"),
-                        r.getString("Name"),
-                        r.getInt("DepartmentID")));
+                "FROM Employee e " +
+                "JOIN Department d " +
+                "WHERE e.DepartmentID = d.ID " +
+                "AND d.Name = 'IT' ", MAP_TO_EMPLOYEE);
 
         System.out.println(employees.size());
     }
